7fadca4b863a698c43594c3c38f14fa40508501d
Loick Adjiwanou <loickadjiwanou1111@gmail.com>
2025-04-08 10:33:02 +0100

    working on Article screen

--- src/screens/private/Article/Article.js
+++ src/screens/private/Article/Article.js
@@ -5,16 +5,27 @@ import {
   TextInput,
   Pressable,
   TouchableOpacity,
+  Image,
+  ScrollView,
 } from "react-native";
 import ArticleStyle from "./Article.style.js";
 import colors from "../../../assets/colors/colors.js";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  Entypo,
+  MaterialCommunityIcons,
+  FontAwesome6,
+} from "@expo/vector-icons";
+import shoes from "../../../assets/images/shoes.png";
+import Swiper from "react-native-swiper";
 
 const Article = (props) => {
   const navigation = useNavigation();
   const route = useRoute();
   const article = route.params?.article;
+  const swiperRef = useRef(null);
+  const [index, setIndex] = useState(0);
+  const [selectedSize, setSelectedSize] = useState("7UK");
 
   return (
     <View style={ArticleStyle.view}>
@@ -33,6 +44,116 @@ const Article = (props) => {
           <MaterialCommunityIcons name="cart-outline" size={24} color="black" />
         </TouchableOpacity>
       </View>
+
+      <ScrollView
+        overScrollMode="never"
+        showsVerticalScrollIndicator={false}
+        vertical
+      >
+        <View style={ArticleStyle.swiper}>
+          <Swiper
+            ref={swiperRef}
+            style={ArticleStyle.wrapper}
+            showsButtons={false}
+            loop={false}
+            onIndexChanged={(index) => setIndex(index)}
+            dot={<View style={ArticleStyle.dot} />}
+            activeDot={<View style={ArticleStyle.activeDot} />}
+          >
+            <Image source={shoes} style={ArticleStyle.image} />
+            <Image source={shoes} style={ArticleStyle.image} />
+            <Image source={shoes} style={ArticleStyle.image} />
+            <Image source={shoes} style={ArticleStyle.image} />
+            <Image source={shoes} style={ArticleStyle.image} />
+          </Swiper>
+        </View>
+
+        <View>
+          <Text style={ArticleStyle.size}>Size: 7UK</Text>
+          <View style={ArticleStyle.sizeBoxs}>
+            <TouchableOpacity
+              onPress={() => setSelectedSize("6UK")}
+              style={[
+                ArticleStyle.sizeBox,
+                selectedSize === "6UK" && { backgroundColor: colors.red },
+              ]}
+            >
+              <Text
+                style={[
+                  ArticleStyle.sizeText,
+                  selectedSize === "6UK" && { color: colors.white },
+                ]}
+              >
+                6 UK
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setSelectedSize("7UK")}
+              style={[
+                ArticleStyle.sizeBox,
+                selectedSize === "7UK" && { backgroundColor: colors.red },
+              ]}
+            >
+              <Text
+                style={[
+                  ArticleStyle.sizeText,
+                  selectedSize === "7UK" && { color: colors.white },
+                ]}
+              >
+                7 UK
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setSelectedSize("8UK")}
+              style={[
+                ArticleStyle.sizeBox,
+                selectedSize === "8UK" && { backgroundColor: colors.red },
+              ]}
+            >
+              <Text
+                style={[
+                  ArticleStyle.sizeText,
+                  selectedSize === "8UK" && { color: colors.white },
+                ]}
+              >
+                8 UK
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setSelectedSize("9UK")}
+              style={[
+                ArticleStyle.sizeBox,
+                selectedSize === "9UK" && { backgroundColor: colors.red },
+              ]}
+            >
+              <Text
+                style={[
+                  ArticleStyle.sizeText,
+                  selectedSize === "9UK" && { color: colors.white },
+                ]}
+              >
+                9 UK
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setSelectedSize("10UK")}
+              style={[
+                ArticleStyle.sizeBox,
+                selectedSize === "10UK" && { backgroundColor: colors.red },
+              ]}
+            >
+              <Text
+                style={[
+                  ArticleStyle.sizeText,
+                  selectedSize === "10UK" && { color: colors.white },
+                ]}
+              >
+                10 UK
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
--- src/screens/private/Article/Article.style.js
+++ src/screens/private/Article/Article.style.js
@@ -21,6 +21,60 @@ const ArticleStyle = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     paddingHorizontal: 5,
+    marginBottom: 20,
+  },
+  swiper: {
+    height: 270,
+    width: "100%",
+  },
+  dotIcon: {
+    marginHorizontal: 5,
+  },
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 15,
+    marginHorizontal: 2,
+    backgroundColor: colors.lightgray,
+    top: 15,
+  },
+  activeDot: {
+    width: 10,
+    height: 10,
+    borderRadius: 15,
+    backgroundColor: colors.red,
+    marginHorizontal: 2,
+    top: 15,
+  },
+  image: {
+    width: "100%",
+    height: 240,
+    borderRadius: 20,
+  },
+  size: {
+    fontFamily: "Montserrat",
+    fontSize: 14,
+    color: colors.black,
+  },
+  sizeBox: {
+    borderWidth: 2,
+    borderColor: colors.red,
+    width: 50,
+    height: 35,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 12,
+  },
+  sizeText: {
+    fontFamily: "Montserrat",
+    fontSize: 14,
+    color: colors.red,
+  },
+  sizeBoxs: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
   },
 });
 export default ArticleStyle;
